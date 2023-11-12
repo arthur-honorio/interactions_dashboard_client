6c1d79d5aaa0bd0915d0de1efafb56afd2675e66
Arthur Honorio <hno.arthur@gmail.com>
2023-11-11 21:35:55 -0300

    Atualizar o Video.tsx

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,62 +1,51 @@
 import { useEffect, useRef } from "react";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
-
 import style from "./Video.module.css";
 
 export function Video({
   showVideo,
   url,
-  toogleShowVideo,
+  toggleShowVideo, // Corrigi o nome da função para "toggleShowVideo"
 }: {
   showVideo: boolean;
   url: string | undefined;
-  toogleShowVideo: () => void;
+  toggleShowVideo: () => void;
 }) {
   const videoRef = useRef(null);
+
   useEffect(() => {
+    const handleCloseOnEscVideo = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        toggleShowVideo();
+      }
+    };
+
     if (showVideo) {
       window.addEventListener("keydown", handleCloseOnEscVideo);
-      window.addEventListener("mousedown", handleClickOutside);
     }
+
     return () => {
       window.removeEventListener("keydown", handleCloseOnEscVideo);
-      window.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [showVideo]);
+  }, [showVideo, toggleShowVideo]);
+
+  // Overlay JSX
+  const overlay = showVideo ? (
+    <div className={style["overlay"]} onClick={toggleShowVideo} />
+  ) : null;
 
   return (
-    <div className={style["video-container"]}>
-      <button>{<FontAwesomeIcon icon={faClose} />}</button>
-      <video autoPlay controls ref={videoRef}>
-        <source src={url} type="video/mp4" />
-      </video>
-    </div>
+    <>
+      {overlay}
+      <div className={`${style["video-container"]} ${showVideo ? style["active"] : ""}`} ref={videoRef}>
+        <button onClick={toggleShowVideo}>
+          <FontAwesomeIcon icon={faClose} />
+        </button>
+        <video autoPlay controls>
+          <source src={url} type="video/mp4" />
+        </video>
+      </div>
+    </>
   );
-
-  function handleCloseOnEscVideo(this: Window, e: KeyboardEvent) {
-    if (showVideo && e.key == "Escape") {
-      toogleShowVideo();
-    }
-  }
-
-  function handleClickOutside(e: MouseEvent) {
-      if (videoRef?.current && e.target) {
-        const rect as {
-          left: number
-          right: number
-          bottom: number
-          top: number
-        } = videoRef.current.getBoundingClientRect();
-        if (
-          e.clientX < rect.left ||
-          e.clientX > rect.right ||
-          e.clientY < rect.top ||
-          e.clientY > rect.bottom
-        ) {
-          toogleShowVideo();
-        }
-      }
-    }
 }
